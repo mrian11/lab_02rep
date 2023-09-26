bea14fcb6569754f94ad0f5af8debdff9ee3852d
mrian11 <marian.yatsyk.ri.2023@lpnu.ua>
2023-09-26 22:01:47 +0300

    Laba_02

--- PR2/Source.cpp
+++ PR2/Source.cpp
@@ -10,13 +10,13 @@ int main()
 {
 	double a;//вхідний параметр
 	double z1;//результат обчислення 1-го виразу
-	double z2;///результат обчислення 2-го виразу
+	//double z2;///результат обчислення 2-го виразу
 	cout << "a = "; cin >> a;
 	z1 = 1.0 - (0.25 * ((sin((2*a)) * sin((2*a))))) + cos(2*a);
-	z2 = ((cos(a) * cos(a))) + (((cos(a) * cos(a)) * cos(a)) * cos(a));
+	//z2 = ((cos(a) * cos(a))) + (((cos(a) * cos(a)) * cos(a)) * cos(a));
 	cout << endl;
 		cout << "z1 = " << z1 << endl;
-		cout << "z2 = " << z2 << endl;
+		//cout << "z2 = " << z2 << endl;
 		cin.get();
 		return 0;
 }
