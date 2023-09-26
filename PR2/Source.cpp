c18c3f15e69da5320a888aa31339938ed243f2cd
mrian11 <marian.yatsyk.ri.2023@lpnu.ua>
2023-09-26 18:43:52 +0300

    Laba 02

---
+++ PR2/Source.cpp
@@ -0,0 +1,22 @@
+//Lab_02.cpp
+// <Yatsyk Marian>
+//Лабораторна робота №2
+// Лінійні програми
+// Варіант 0.1
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double a;//вхідний параметр
+	double z1;//результат обчислення 1-го виразу
+	double z2;//результат обчислення 2-го виразу
+	cout << "a = "; cin >> a;
+	z1 = 1.0 - (0.25 * ((sin((2) * (a)) * sin((2) * (a))))) + cos(2) * (a);
+	z2 = ((cos(a) * cos(a))) + (((cos(a) * cos(a)) * cos(a)) * cos(a));
+	cout << endl;
+		cout << "z1 = " << z1 << endl;
+		cout << "z2 = " << z2 << endl;
+		cin.get();
+		return 0;
+}
